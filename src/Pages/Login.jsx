041c4542ea91b6eecade56ea9050c1a6c7869a40
lawrencemasilo <neolawrencemasilo@gmail.com>
2024-02-29 20:55:38 +0200

    Implemented ability to signIn with Google

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,14 @@
-import {signInWithEmailAndPassword } from "firebase/auth";
+import {GoogleAuthProvider, signInWithEmailAndPassword, signInWithPopup } from "firebase/auth";
 import { auth } from "../firebase";
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import googleImg from "../images/google.svg"
 
 function Login() {
   const [inputValue, setInputValue] = useState(null);
   const [err, setErr] = useState(false);
   const navigate = useNavigate();
+  const provider = new GoogleAuthProvider();
 
   const handleSubmit = async (e) => {
     /* passes the credentials to the user authentication system in the backend */
@@ -27,6 +29,22 @@ function Login() {
 
   const handleChange = (e) => {
     setInputValue(e.target.value);
+  }
+
+  const handleClick = () => {
+    signInWithPopup(auth, provider)
+    .then(() => {
+      navigate("/home")
+    }).catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+    });
   }
   return (
     <div className="limiter">
@@ -56,24 +74,39 @@ function Login() {
               <span className="focus-input"></span>
             </div>
             {err && <p className="err">Incorrect username or password</p>}
+            
             <div className="container-login-form-btn">
               <button className="login-form-btn">
                 Login
               </button>
             </div>
 
-            <div className="text-center">
+            <div className="text-center p-t-136">
               <span className="txt1">
                 Forgot
               </span>
-              <a className="txt2" href="#">
-              <Link to="/forgot">Username / Password?</Link>
-              </a>
+              <p className="txt2" href="#">
+              <Link to="/forgot" className="signIn-link">Password?</Link>
+              </p>
+            </div>
+
+            <div className="alt-login-container">
+              <div className="separater1"></div>
+              <p className="or">OR</p>
+              <div className="separater2"></div>
+            </div>
+            <div className="alt-providers">
+              <div className="google-provider">
+                <img src={googleImg} alt="google icon" onClick={handleClick} />
+              </div>
             </div>
 
             <div className="text-center p-t-136">
+              <span className="txt1">
+                Don't have an account?
+              </span>
               <p className="txt2" href="#">
-                <Link to="/signup">Create your Account</Link>
+                <Link to="/signup" className="signIn-link">Sign Up Now!</Link>
               </p>
             </div>
           </form>
